0ae53ea42a7fc306977df86a6473cdbefbcb650c
Josh Hawkins <hawkins@users.noreply.github.com>
2018-12-13 20:27:14 -0500

    WIP: Add lambda bootstrap
    Does not compile on my mac, but if one had OpenSSL and musl-gcc set up
    right, I presume it would

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
+#[macro_use]
+extern crate lambda_runtime as lambda;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate log;
 extern crate regex;
 extern crate reqwest;
 extern crate select;
+extern crate simple_logger;
 
+use lambda::error::HandlerError;
 use regex::Regex;
 use select::document::Document;
 use select::predicate::{Class, Name, Predicate};
+use std::error::Error;
 use std::vec::Vec;
+
+#[derive(Deserialize, Clone)]
+struct CustomEvent {
+    #[serde(rename = "firstName")]
+    first_name: String,
+}
+
+#[derive(Serialize, Clone)]
+struct CustomOutput {
+    message: String,
+}
 
 fn scrape() -> Result<Vec<(String, String)>, Box<std::error::Error>> {
     let body = reqwest::get("https://www.districtdoughnut.com")?.text()?;
@@ -30,9 +50,25 @@ fn scrape() -> Result<Vec<(String, String)>, Box<std::error::Error>> {
     Ok(flavors)
 }
 
-fn main() {
+fn my_handler(e: CustomEvent, c: lambda::Context) -> Result<CustomOutput, HandlerError> {
     match scrape() {
         Ok(c) => println!("Pass: {}", c.len()),
         Err(e) => println!("Fail: {}", e.to_string()),
     }
+
+    if e.first_name == "" {
+        error!("Empty first name in request {}", c.aws_request_id);
+        return Err(c.new_error("Empty first name"));
+    }
+
+    Ok(CustomOutput {
+        message: format!("Hello, {}!", e.first_name),
+    })
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_level(log::Level::Info)?;
+    lambda!(my_handler);
+
+    Ok(())
 }
